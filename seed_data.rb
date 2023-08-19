6cb7606f763c8deffe235e0ce46bf3deee7cda92
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-19 00:44:21 -0700

    processing data in models

---
+++ seed_data.rb
@@ -0,0 +1,9 @@
+# seed_data.rb
+
+# Your existing code for creating users and posts
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+
+# Command to create post comments
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
