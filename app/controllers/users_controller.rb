fd03d57514821bd7a27a6ca684492d30c423a5bf
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-21 17:54:51 -0700

    create routes, controllers and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    def show
+      @user = User.find(params[:id])
+    end
+  
+    def posts
+      @user = User.find(params[:user_id])
+    end
+  
+    def post
+      @user = User.find(params[:user_id])
+      @post = Post.find(params[:post_id])
+    end
+  end  
