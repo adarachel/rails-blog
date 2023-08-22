fc087fb0483977ac8382d92445b74548105434a0
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-21 18:26:37 -0700

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,9 @@
 #       @user = User.find(params[:user_id])
 #       @posts = @user.posts
 #     end
-  
+
 #     def show
 #       @user = User.find(params[:user_id])
 #       @post = Post.find(params[:post_id])
 #     end
 #   end
-  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-    def show
-      @user = User.find(params[:id])
-    end
-  
-    def posts
-      @user = User.find(params[:user_id])
-    end
-  
-    def post
-      @user = User.find(params[:user_id])
-      @post = Post.find(params[:post_id])
-    end
-  end  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def posts
+    @user = User.find(params[:user_id])
+  end
+
+  def post
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+  end
+end
