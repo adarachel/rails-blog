cb5d11603ff9d4b4c651459c4a667f4c5ba59b7d
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-24 14:38:32 -0700

    get tests to pass!

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails-controller-testing'
 
+gem 'factory_bot_rails'
+
+gem 'faker'
+
 gem 'rspec'
 
 gem 'rubocop', '>= 1.0', '< 2.0'
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -20,5 +20,26 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
+  describe 'GET /users' do
+    it 'renders the index template' do
+      get '/users' # Directly specify the path
+      expect(response).to render_template(:index)
+    end
+
+    it 'has a 200 status code' do
+      get '/users' # Directly specify the path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'includes correct user names' do
+      user1 = User.create(name: 'Alice')
+      user2 = User.create(name: 'Bob')
+
+      get '/users' # Directly specify the path
+      expect(response.body).to include(user1.name)
+      expect(response.body).to include(user2.name)
+    end
+  end
+
   # Similar specs for other actions...
 end
