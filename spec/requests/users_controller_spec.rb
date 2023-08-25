1bece0969d836f76884372f23cd217819b53d170
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-24 17:04:33 -0700

    add tests for index action

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -5,37 +5,45 @@ RSpec.describe 'Users', type: :request do
     let(:user) { User.create(name: 'John Doe') }
 
     it 'renders the show template' do
-      get "/users/#{user.id}" # Directly specify the path
+      get "/users/#{user.id}"
       expect(response).to render_template(:show)
     end
 
     it 'has a 200 status code' do
-      get "/users/#{user.id}" # Directly specify the path
+      get "/users/#{user.id}"
       expect(response).to have_http_status(200)
     end
 
     it 'includes correct placeholder text' do
-      get "/users/#{user.id}" # Directly specify the path
+      get "/users/#{user.id}"
       expect(response.body).to include(user.name)
     end
   end
 
   describe 'GET /users' do
     it 'renders the index template' do
-      get '/users' # Directly specify the path
+      get '/users'
       expect(response).to render_template(:index)
     end
 
     it 'has a 200 status code' do
-      get '/users' # Directly specify the path
+      get '/users'
       expect(response).to have_http_status(200)
+    end
+
+    it 'assigns @users with all users' do
+      user1 = User.create(name: 'Alice')
+      user2 = User.create(name: 'Bob')
+
+      get '/users'
+      expect(assigns(:users)).to match_array([user1, user2])
     end
 
     it 'includes correct user names' do
       user1 = User.create(name: 'Alice')
       user2 = User.create(name: 'Bob')
 
-      get '/users' # Directly specify the path
+      get '/users'
       expect(response.body).to include(user1.name)
       expect(response.body).to include(user2.name)
     end
