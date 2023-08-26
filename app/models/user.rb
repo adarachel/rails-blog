972baea0bf812621b65f89fd71096426e033bd4c
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-25 17:05:37 -0700

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-  
+
   def posts_counter
     posts.count
   end
