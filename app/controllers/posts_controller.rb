7c56c8db22667e2be75e91ad8f19a662c5106a5f
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-08-31 21:02:39 -0700

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,32 @@
 class PostsController < ApplicationController
-    def index
-      @user = User.find(params[:user_id])
-      @posts = @user.posts.includes(:comments)
-      @like = current_user.likes.new
-    end
-  
-    def show
-      @user = User.find(params[:user_id])
-      @post = Post.find_by!(id: params[:id], author_id: params[:user_id])
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = current_user.posts.new(post_params)
-  
-      if @post.save
-        redirect_to user_post_path(@post.author, @post)
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def post_params
-      params.require(:new_post).permit(:title, :text)
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+    @like = current_user.likes.new
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find_by!(id: params[:id], author_id: params[:user_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :new
     end
   end
+
+  private
+
+  def post_params
+    params.require(:new_post).permit(:title, :text)
+  end
+end
