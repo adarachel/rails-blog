5f3984c10114256520781281fdf2196c29dfb4e9
Ada Rachel <108772486+adarachel@users.noreply.github.com>
2023-09-01 13:38:42 -0700

    Install and setup devise
    Co-Authored-By: Dilsher Balouch <dilsherb@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+userOne = User.create(name: "Raquel", photo: "https://images.unsplash.com/photo-1692842717781-022ea74e2498?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60", bio: "raquel", email: "raquelcee@gmail.com", password: "12345678")
+userTwo = User.create(name: "Raquel", photo: "https://images.unsplash.com/photo-1692842717781-022ea74e2498?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60", bio: "raquel", email: "raquelcee@gmail.com", password: "12345678")
+userThree = User.create(name: "Raquel", photo: "https://images.unsplash.com/photo-1692842717781-022ea74e2498?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60", bio: "raquel", email: "raquelcee@gmail.com", password: "12345678")
+userFour = User.create(name: "Raquel", photo: "https://images.unsplash.com/photo-1692842717781-022ea74e2498?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60", bio: "raquel", email: "raquelcee@gmail.com", password: "12345678")
+userFive = User.create(name: "Raquel", photo: "https://images.unsplash.com/photo-1692842717781-022ea74e2498?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60", bio: "raquel", email: "raquelcee@gmail.com", password: "12345678")
